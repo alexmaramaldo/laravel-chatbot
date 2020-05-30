3121853497e23aa74c988d0aab4d3b9457fef1b6
Alex Maramaldo <alex@osbox.com.br>
2020-05-30 18:26:49 -0300

    adding meta tag do disable zoom on ios safari

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"/>
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
