3cc14198e858a80c7238c92679e642fa553b9c86
Alex Maramaldo <alex@osbox.com.br>
2020-05-31 11:51:26 -0300

    removing comments

--- routes/chatbot.php
+++ routes/chatbot.php
@@ -20,32 +20,11 @@ use Illuminate\Support\Facades\Auth;
 $botman = resolve('botman');
 
 $botman->hears('Hi|Hello|Hey', function ($bot) {
-    $bot->reply("Hello " . Auth::user()['name'] . "! My name is Botman and I will help you with the transactions.");
+    $bot->reply("Hello " . Auth::user()['name'] . "! My name is BotBanker, How Can I help you?.");
 });
 
 $botman->hears('{message}', 'App\Http\Controllers\CommandController@actionCommands');
 
-// $botman->hears('Available currencies', 'App\Http\Controllers\CurrencyController@availableCurrencies');
-// $botman->hears('Set default currency {currency}', 'App\Http\Controllers\CurrencyController@setDefault');
-// $botman->hears('Deposit {value}', 'App\Http\Controllers\AccountController@deposit');
-// $botman->hears('Withdraw {value}', 'App\Http\Controllers\AccountController@withdraw');
-// $botman->hears('Show account balance', 'App\Http\Controllers\AccountController@accountBalance');
-
 $botman->fallback(function ($bot) {
     $bot->reply("Unknown command.");
 });
-
-
-// Route::group([
-
-//     'middleware' => 'api',
-//     'prefix' => 'auth'
-
-// ], function ($router) {
-
-//     Route::post('login', 'AuthController@login');
-//     Route::post('logout', 'AuthController@logout');
-//     Route::post('refresh', 'AuthController@refresh');
-//     Route::post('me', 'AuthController@me');
-
-// });
